249573979513178326aa0453ce8a482f76498a5f
anatoliyBannikov <anatoliy.bannikov.business@gmail.com>
2025-04-10 18:06:48 +0500

    dz4 added banalce/history endpoint

--- app/infra/api/balance.py
+++ app/infra/api/balance.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
 
 from app.infra.database.session import get_db
 from app.infra.database.crud.user_crud import get_user_by_username
+from app.infra.database.crud.transactionlogs_crud import get_transaction_logs_by_user
 from app.infra.services.balance_manager import BalanceManagerORM
-from app.schemas.sch_balance import BalanceUpdateRequest, BalanceResponse
+from app.schemas.sch_balance import (
+    BalanceUpdateRequest, 
+    BalanceResponse,
+    TransactionLogOut
+)
 from app.domain.enums import TransactionSource
 
 router = APIRouter()
@@ -39,3 +45,11 @@ def top_up_balance(
     bm.add_balance(amount=data.amount, source=data.source)
     db.commit()
     return {"balance": bm.get_balance()}
+
+
+@router.get("/history", response_model=List[TransactionLogOut])
+def get_transaction_history(
+    db: Session = Depends(get_db),
+    user=Depends(get_mock_user)
+):
+    return get_transaction_logs_by_user(db, user.id)
