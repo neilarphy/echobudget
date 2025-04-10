3e0eeaa086c17074ef81925fd6a9e24b2b912fa9
anatoliyBannikov <anatoliy.bannikov.business@gmail.com>
2025-04-10 18:24:58 +0500

    dz4 added balance deduction into predict endpoint

--- app/infra/api/predict.py
+++ app/infra/api/predict.py
@@ -5,6 +5,8 @@ from app.infra.database.session import get_db
 from app.infra.database.crud.user_crud import get_user_by_username
 from app.schemas.sch_predict import PredictRequest, PredictResponse
 from app.domain.ml_factory import MLModelFactory
+from app.infra.services.balance_manager import BalanceManagerORM
+from app.domain.enums import TransactionSource
 
 router = APIRouter()
 
@@ -27,11 +29,18 @@ def make_prediction(
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+    bm = BalanceManagerORM(db=db, user=user)
+    success, _ = bm.deduct_balance(amount=model.get_cost(), source=TransactionSource.INFERENCE)
+    if not success:
+        raise HTTPException(status_code=403, detail="Недостаточно средств для проведение предсказания")
+    
     try:
         result = model.predict(request.data)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Prediction error: {e}")
 
+    db.commit()
+    
     return PredictResponse(
         result=result,
         model_used=request.model_type,
